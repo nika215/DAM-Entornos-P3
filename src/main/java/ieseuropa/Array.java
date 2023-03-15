d99e28286149e71a56ea8e571ec0d1f9a4053dc1
adrian <adrian@DPTO-PC1>
2023-03-15 18:45:30 +0100

    Revert "aaaa"
    This reverts commit 34e5b3da63ea18514d83f36f266e39f26d8e219f.

--- src/main/java/ieseuropa/Array.java
+++ src/main/java/ieseuropa/Array.java
@@ -2,10 +2,6 @@ package main.java.ieseuropa;
 
 public class Array {
 
-	private static void nada() {
-		//No hago nada
-	}
-	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola");
