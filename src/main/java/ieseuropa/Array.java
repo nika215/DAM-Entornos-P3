34e5b3da63ea18514d83f36f266e39f26d8e219f
adrian <adrian@DPTO-PC1>
2023-03-15 18:31:42 +0100

    aaaa

--- src/main/java/ieseuropa/Array.java
+++ src/main/java/ieseuropa/Array.java
@@ -2,6 +2,10 @@ package main.java.ieseuropa;
 
 public class Array {
 
+	private static void nada() {
+		//No hago nada
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola");
