406fc72a610c45ec6a332f9a9618b320ebb7b21f
Saphi <Saphi@DESKTOP-7D79RVN>
2023-03-20 16:46:43 +0100

    Redondeado a la alza

---
+++ src/main/java/ieseuropa/RedondearAlza.java
@@ -0,0 +1,11 @@
+package main.java.ieseuropa;
+
+public class RedondearAlza {
+
+	public static void main(String[] args) {
+		double numero = 8.52;
+		double redondeado = Math.ceil(numero);
+		System.out.println("El numero redondeado es: ." + redondeado); 
+	}
+
+}
