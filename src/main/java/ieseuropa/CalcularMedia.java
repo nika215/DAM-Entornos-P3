37cc4365f72dd9c43481916027bd97ee401a40c2
Saphi <Saphi@DESKTOP-7D79RVN>
2023-03-20 16:49:25 +0100

    Calcular media con 10 números

--- src/main/java/ieseuropa/CalcularMedia.java
+++ src/main/java/ieseuropa/CalcularMedia.java
@@ -4,7 +4,7 @@ public class CalcularMedia {
 
 	public static void main(String[] args) {
 		
-		int[] numeros = {2,8,28,35,17,80,23,99};
+		int[] numeros = {2,8,28,35,17,80,23,99,12,23};
 		
 		int mayor = 0;
 		int suma = 0;
