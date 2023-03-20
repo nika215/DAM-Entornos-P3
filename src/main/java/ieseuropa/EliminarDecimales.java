f94d688451ad80a733184780042e100d0f8c7030
Saphi <Saphi@DESKTOP-7D79RVN>
2023-03-20 16:39:13 +0100

    Quitar decimales

---
+++ src/main/java/ieseuropa/EliminarDecimales.java
@@ -0,0 +1,13 @@
+package main.java.ieseuropa;
+
+public class EliminarDecimales {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double num = 3.14159;
+		int numEntero = (int) num; 
+		System.out.println(numEntero);
+	}
+
+}
