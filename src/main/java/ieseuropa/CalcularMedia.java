af8f2392f5c406279d644a28f31b5fb7b8d84a1d
Saphi <Saphi@DESKTOP-7D79RVN>
2023-03-20 16:34:17 +0100

    Metodo para calcular la media

---
+++ src/main/java/ieseuropa/CalcularMedia.java
@@ -0,0 +1,19 @@
+package main.java.ieseuropa;
+
+public class CalcularMedia {
+
+	public static void main(String[] args) {
+		
+		int[] numeros = {2,8,28,35,17,80,23,99};
+		
+		int mayor = 0;
+		int suma = 0;
+		for(int i=0;i<numeros.length;i++) {
+			if(numeros[i] > mayor) {
+				mayor = numeros[i];
+			}
+			suma += numeros[i];
+		}
+		System.out.println("La media es " + suma/numeros.length);
+	}
+}
