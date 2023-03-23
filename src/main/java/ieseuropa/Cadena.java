3c34e37524b9cd7cb398aa9db8b2e7da50d68136
OscarDH <chusmita.oscar@gmail.com>
2023-03-23 18:55:35 +0100

    Cambiar a mayusculas

---
+++ src/main/java/ieseuropa/Cadena.java
@@ -0,0 +1,13 @@
+package main.java.ieseuropa;
+
+public class Cadena {
+
+	private static String cambiarMayus(String string) {
+		return string.toUpperCase();
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
