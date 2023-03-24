229a6dbb3814f6db8ae0e3c6f84c6c0c1b6e836c
KIKE <KIKE@DIEZ>
2023-03-24 18:05:34 +0100

    Método para calcular el valor minimo de un Array.

--- src/main/java/ieseuropa/Array.java
+++ src/main/java/ieseuropa/Array.java
@@ -2,9 +2,19 @@ package main.java.ieseuropa;
 
 public class Array {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hola");
+	public static int calcularMinimo(int[] array) {
+		int minimo = array[0];
+		for (int i = 1; i < array.length; i++) {
+			if (array[i] < minimo) {
+				minimo = array[i];
+			}
+		}
+		return minimo;
 	}
 
+	public static void main(String[] args) {
+		int[] array = { 5, 2, 9, 1, 7 };
+		int minimo = Array.calcularMinimo(array);
+		System.out.println("El valor mínimo del array es: " + minimo);
+	}
 }
