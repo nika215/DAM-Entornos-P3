a80b1953ba20595a538b482acb6e4e9035e2fb25
OscarDH <chusmita.oscar@gmail.com>
2023-03-24 18:04:22 +0100

    maximo array

--- src/main/java/ieseuropa/Array.java
+++ src/main/java/ieseuropa/Array.java
@@ -2,17 +2,18 @@ package main.java.ieseuropa;
 
 public class Array {
 
-	private static int maximo(int[]array) {
+	private static int maximo(int[] array) {
 		int max = 0;
-		for(int num : array) {
+		for (int num : array) {
 			if (num > max) {
 				max = num;
 			}
 		}
 		return max;
 	}
+
 	public static void main(String[] args) {
-		int[]array = {1,2,3,4,5,6,7,8,9,10};
+		int[] array = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 		System.out.println(maximo(array));
 	}
 
