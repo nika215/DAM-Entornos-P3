1a685d4268603600b1f07b40087d9463e159ab04
KIKE <KIKE@DIEZ>
2023-03-24 17:55:32 +0100

    Método para convertir a máyusculas un String.

--- src/main/java/ieseuropa/Cadena.java
+++ src/main/java/ieseuropa/Cadena.java
@@ -5,9 +5,20 @@ public class Cadena {
 	private static String cambiarMayus(String string) {
 		return string.toUpperCase();
 	}
+	
+	public static String convertirMinusculas(String nombre) {
+		return nombre.toLowerCase();
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		  String texto1 = "Hola Mundo!";
+	        String texto2 = "ESTOY HARTO DE PRACTICAS!!!"; 
+	        
+	        String resultado1 = cambiarMayus(texto1);
+	        System.out.println(resultado1); 
+	      
+	        String resultado2 = convertirMinusculas(texto2);
+	        System.out.println(resultado2); 
 
 	}
-
 }
