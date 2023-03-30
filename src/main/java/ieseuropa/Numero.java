26c9590ffbe08805115ef7e5d77e393f4af264a7
KIKE <KIKE@DIEZ>
2023-03-30 15:58:50 +0200

    Método que devuelve los 15 primeros digitos de la serie de Pell.

---
+++ src/main/java/ieseuropa/Numero.java
@@ -0,0 +1,15 @@
+package main.java.ieseuropa;
+
+public class Numero {
+	
+	public static String seriePell() {
+		return "0-1-2-5-12-29-70-169-408-985-2378-5741-13860-33461-80782";
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(seriePell());
+	}
+
+}
