4c27ef46693c663ca5c573ad7e10ab2215a96af4
monica <monica.antona@educa.madrid.org>
2023-03-31 18:41:25 +0200

    Agregado mediana

--- src/main/java/ieseuropa/ListaArray.java
+++ src/main/java/ieseuropa/ListaArray.java
@@ -8,7 +8,19 @@ public class ListaArray {
 		Arrays.sort(array);
 		return array;
 	}
-	
+	private static float mediana (int[] array) {
+		array = ordenarArray(array);
+		int size = array.length;
+		float mediana = 0;
+		if(size%2!=0) {
+			mediana=array[(size-1)/2];
+		}
+		else {
+			int posicion=(size/2);
+			mediana=(float)(array[posicion-1]+array[posicion])/2;
+		}
+		return mediana;
+	}
 	public static void main(String[] args) {
 		int[] arrayOrdenar = new int[] {3,20,15,1,9};
 		arrayOrdenar = ordenarArray(arrayOrdenar); 
@@ -16,6 +28,8 @@ public class ListaArray {
 			System.out.print(arrayOrdenar[i]+" ");
 		}
 		System.out.println();
+		
+		System.out.println("Mediana: "+ mediana(arrayOrdenar));
 	}
 
 }
